e46e8a90de4fc9d22e06386bb7f510d5564d74e3
Rinalds Vīksna <rinalds.viksna@Tilde.lv>
2021-04-22 13:58:41 +0300

    hide complexity in ScoringTool class

--- Readme.md
+++ Readme.md
@@ -1,18 +1,31 @@
-Formāts - Dockers  
-Interface - web forma tornado  
-Crawleris - Scrapy  
-Boilerplate - ?  
-Lang detect - ?  
+Format - Dockers  
+Interface - web forma   
+Crawler - Scrapy  
+Boilerplate - justext/trafilatura/html_text  
+Lang detect - langdetect/fasttext   
 Glue code - python  
 
-Ieejā saraksts ar domēniem (var kā failu)  
-Izejā saraksts ar scorēm (var failā) + detalizēts reports  
+Input list of domain names (possibly as file)  
+Output list of scores (possibly as file) + detailed report  
 
 
 # Dependencies
 pip install scrapy  
 pip install tldextract  
-pip install pybind11  
-pip install fasttext  
-pip install justext OR pip install trafilatura  for text extraction/boilerplate removal  
+pip install justext OR pip install trafilatura OR pip install html_text  for text extraction/boilerplate removal  
+pip install langdetect OR pip install pybind11 + pip install fasttext   
 
+python=3.9.4
+Scrapy-2.5.0
+tldextract-3.1.0
+jusText-2.2.0
+trafilatura-0.8.2
+html_text-0.5.2
+langdetect-1.0.8
+
+# Additional note 
+Langdetect needs irish, maltese and korean model files updated:  
+copy files from "dist\langdetect\profiles\"  
+to folder where langdetect is installed in your system.  
+On windows it will be something like  
+"C:\ProgramData\Anaconda3\envs\python3.8\Lib\site-packages\langdetect\profiles\"
--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import os
 import shutil
 import sys
+import threading
+
+# import tornado.ioloop
+# import tornado.web
+
 from urllib.parse import urlparse
 import configparser
 # pip install scrapy # Use version 2.4.0 # https://github.com/scrapy/scrapy/blob/master/LICENSE
 import scrapy
 from scrapy.crawler import CrawlerProcess
-from modules.spider import ScoringSpider
-from modules.spider import ScoringSpiderSitemap
-from modules.analyzer import Analyzer
-from modules.reporter import Reporter
-from modules.common_functions import extractDomain
-
 from scrapy.utils.project import get_project_settings
+
+from modules.scoring_tool import ScoringTool
+
 
 def run_scoring(urls=None):
     if urls == None:
@@ -28,74 +30,14 @@ def run_scoring(urls=None):
             # 'https://luxexpress.com',
             # 'https://aerodium.technology',
             # 'https://www.madaracosmetics.com',
-            'https://www.airbaltic.com',
+            # 'https://www.airbaltic.com',
             # 'https://census.gov.uk/help/languages-and-accessibility/languages',
 
         ]
-    sitemap_urls = []
-    for url in urls:
-        parsed_url = urlparse(url)
-        sitemap_urls.append(f'{parsed_url.scheme}://{parsed_url.netloc}/sitemap.xml')
-        sitemap_urls.append(f'{parsed_url.scheme}://{parsed_url.netloc}/robots.txt')
 
-    allowed_domains = [extractDomain(i) for i in urls]
-    # print(f'\nPrepared allowed_domains {allowed_domains}\n')
-
-    # settings = {}
-    settings = get_project_settings()
-
-    config = configparser.ConfigParser()
-    config.read('settings.ini')
-    def override_default_crawler_config():
-        for key in config['crawler']:
-            settings[key.upper()] = config.get('crawler', key, fallback='')
-    override_default_crawler_config()
-
-    # Dump config to file for debug
-    with open('settings.cfg', 'w', encoding='utf-8') as of:
-        for key, value in settings.items():
-            of.write(f'{key}, {value}\n')
-
-    # Use new log file for each run
-    try:
-        os.remove(settings['LOG_FILE'])
-    except PermissionError:
-        pass
-    except FileNotFoundError:
-        pass
-
-    analyzer_data_dir = config.get('analyzer', 'data_dir', fallback='')
-    # Clean analyzed_dir before running
-    try:
-        shutil.rmtree(analyzer_data_dir)
-    except Exception as e:
-        print("Exception "+str(e))
-        pass
-
-    process = CrawlerProcess(settings=settings)
-
-    spider = ScoringSpider  # CrawlerProcess accepts Spider class or Crawler instances
-    sitemapspider = ScoringSpiderSitemap
-
-    spider.start_urls = urls
-    spider.allowed_domains = allowed_domains 
-    spider.analyzer = Analyzer(analyzer_data_dir)
-    sitemapspider.sitemap_urls = sitemap_urls
-    sitemapspider.analyzer = spider.analyzer
-
-
-    process.crawl(sitemapspider)
-    process.crawl(spider)
-    process.start()
-    # process.stop()
-
-    reporter = Reporter(analyzer_data_dir)
-    for domain in allowed_domains:
-        stats = reporter.get_stats(domain)
-        # stats: 'language_balance', 'lang_count', 'langs', # For full list see Reporter
-        score = reporter.get_score_from_stats(stats)
-        print("Domain: {}, score: {:0.3f}, langs: {}".format(domain, score, stats['langs']))
-
+    scorer = ScoringTool()
+    
+    score = scorer.initialize(urls)
 
     print("Done crawling ")
     return score    # returns last score for testing
