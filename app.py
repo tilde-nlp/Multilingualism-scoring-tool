7266040a18e1262491a660fe3ee08c7168ef9162
Rinalds Vīksna <rinalds.viksna@Tilde.lv>
2021-05-06 11:28:30 +0300

    Web backend v0.1. Tests need fixing

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import os
 import shutil
 import sys
+import json
 import threading
+from multiprocessing import Process
 
-# import tornado.ioloop
-# import tornado.web
+import signal
+import tornado.ioloop
+import tornado.web
+from twisted.internet import reactor
 
 from urllib.parse import urlparse
 import configparser
@@ -15,38 +19,105 @@ from scrapy.utils.project import get_project_settings
 
 from modules.scoring_tool import ScoringTool
 
+class ScoringHandler(tornado.web.RequestHandler):
+    def initialize(self, scorer):
+        self.scorer = scorer
 
-def run_scoring(urls=None):
-    if urls == None:
-        urls = [
-            # 'https://www.bmw.com/',  # quite mulitlingual
-            # 'https://www.memorywater.com/', # 19 links l2, 3 langs 
-            # 'https://www.tilde.lv/',    # mulitlingual .ee .lt
-            'http://gorny.edu.pl/', # 5 links l2, mono
-            # 'https://www.norden.org/',
-            # 'https://europa.eu/',
-            # 'https://globalvoices.org/',
-            # 'https://www.royalfloraholland.com',
-            # 'https://luxexpress.com',
-            # 'https://aerodium.technology',
-            # 'https://www.madaracosmetics.com',
-            # 'https://www.airbaltic.com',
-            # 'https://census.gov.uk/help/languages-and-accessibility/languages',
+    def post(self):
+        # API v.1
+        #     start_crawl(urls, settings={hopi,})
+        #         starts crawling of urls using settings
+        #         returns "Started crawling of {len(urls)} urls."
+        #     get_crawl_progress_status()
+        #         ? return current depth of crawling for each url ?
+        #     get_current_scores()
+        #         returns statistics&scores at this moment
+        #     stop_crawl()
+        #         stops crawling and returns get_current_scores()
+        q = self.get_query_argument("q", "", False)
+        if q == "start_crawl":
+            urls = self.get_body_argument("urls", default=None, strip=False)
+            hops = self.get_body_argument("hops", default=None, strip=False)
+            urls = urls.split(",")
+            hops = int(hops)
+            print(f"Urls: {urls}")
+            print(f"hops: {hops}")
 
-        ]
+            self.scorer.initialize4thr(urls)
+            response = self.scorer.do_crawling_in_separate_thread()
+            self.write(json.dumps(f"Started crawling of {len(urls)} urls.", indent=2, ensure_ascii=False))
+        elif q == "get_crawl_progress_status":
+            response = self.scorer.get_current_stats()
+            self.write(json.dumps(response, indent=2, ensure_ascii=False))
+        elif q == "get_current_scores":
+            response = self.scorer.get_current_stats()
+            self.write(json.dumps(response, indent=2, ensure_ascii=False))
+        elif q == "stop_crawl":
+            pass
+        elif q == "quit":
+            self.write(json.dumps("Exiting", indent=2, ensure_ascii=False))
+            print("Exiting")
+            reactor.stop()
+            sys.exit()
+
+def make_app(scorer):
+    return tornado.web.Application([
+        (r"/score", ScoringHandler, {"scorer": scorer}),
+    ])
+
+def sig_exit(signum, frame):
+    tornado.ioloop.IOLoop.instance().add_callback_from_signal(do_stop)
+
+def do_stop():
+    print("Trying to stop")
+    tornado.ioloop.IOLoop.instance().stop()
+
+
+def run_scoring_web():
+    PORT = '8989'
+    ADDRESS = '127.0.0.127'
 
     scorer = ScoringTool()
-    
-    score = scorer.initialize(urls)
+    app = make_app(scorer)
 
-    print("Done crawling ")
-    return score    # returns last score for testing
+    app.listen(PORT, address = ADDRESS)
+    signal.signal(signal.SIGINT, sig_exit)
+    print(f"Server started on {ADDRESS}:{PORT}!")
+    tornado.ioloop.IOLoop.current().start()
+
+
+# def run_scoring(urls=None):
+#     if urls == None:
+#         urls = [
+#             # 'https://www.bmw.com/',  # quite mulitlingual
+#             # 'https://www.memorywater.com/', # 19 links l2, 3 langs 
+#             # 'https://www.tilde.lv/',    # mulitlingual .ee .lt
+#             'http://gorny.edu.pl/', # 5 links l2, mono
+#             # 'https://www.norden.org/',
+#             # 'https://europa.eu/',
+#             # 'https://globalvoices.org/',
+#             # 'https://www.royalfloraholland.com',
+#             # 'https://luxexpress.com',
+#             # 'https://aerodium.technology',
+#             # 'https://www.madaracosmetics.com',
+#             # 'https://www.airbaltic.com',
+#             # 'https://census.gov.uk/help/languages-and-accessibility/languages',
+
+#         ]
+
+#     scorer = ScoringTool()
+    
+#     score = scorer.initialize(urls)
+
+#     print("Done crawling ")
+#     return score    # returns last score for testing
 
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description='This is tool for evaluation of website multilinguality!')
-    parser.add_argument('-u', '--urls', nargs='+', help='Starting urls to evaluate, domains will be extracted from them.')
-    args = parser.parse_args()
-    urls = args.urls
-    run_scoring(urls)
+    # import argparse
+    # parser = argparse.ArgumentParser(description='This is tool for evaluation of website multilinguality!')
+    # parser.add_argument('-u', '--urls', nargs='+', help='Starting urls to evaluate, domains will be extracted from them.')
+    # args = parser.parse_args()
+    # urls = args.urls
+    # run_scoring(urls)
+    run_scoring_web()
--- modules/scoring_tool.py
+++ modules/scoring_tool.py
@@ -7,85 +7,207 @@ from urllib.parse import urlparse
 import configparser
 # pip install scrapy # Use version 2.4.0 # https://github.com/scrapy/scrapy/blob/master/LICENSE
 import scrapy
-from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import CrawlerProcess, CrawlerRunner
 from modules.spider import ScoringSpider
 from modules.spider import ScoringSpiderSitemap
 from modules.analyzer import Analyzer
 from modules.reporter import Reporter
 from modules.common_functions import extractDomain
 
+from twisted.internet import reactor, defer
+
 from scrapy.utils.project import get_project_settings
 
 class ScoringTool():
+    # Control scrapy crawling, and Reporting 
+    # def initialize(self, urls):
+    #     print("Initializing ScoringTool class")
+    #     self.urls = urls
 
-    def initialize(self, urls):
+    #     sitemap_urls = []
+    #     for url in urls:
+    #         parsed_url = urlparse(url)
+    #         sitemap_urls.append(f'{parsed_url.scheme}://{parsed_url.netloc}/sitemap.xml')
+    #         sitemap_urls.append(f'{parsed_url.scheme}://{parsed_url.netloc}/robots.txt')
+
+    #     # settings = {}
+    #     settings = get_project_settings()
+
+    #     config = configparser.ConfigParser()
+    #     config.read('settings.ini')
+    #     def override_default_crawler_config():
+    #         for key in config['crawler']:
+    #             settings[key.upper()] = config.get('crawler', key, fallback='')
+    #     override_default_crawler_config()
+
+    #     self.analyzer_data_dir = config.get('analyzer', 'data_dir', fallback='')
+    #     self.reporter = Reporter(self.analyzer_data_dir)
+    #     self.allowed_domains = [extractDomain(i) for i in urls]
+    #     # print(f'\nPrepared allowed_domains {allowed_domains}\n')
+
+    #     def dump_config_to_file_for_debug():
+    #         with open('settings.cfg', 'w', encoding='utf-8') as of:
+    #             for key, value in settings.items():
+    #                 of.write(f'{key}, {value}\n')
+    #     dump_config_to_file_for_debug()
+
+    #     def clear_log_file(): # Use new log file for each run
+    #         try:
+    #             os.remove(settings['LOG_FILE'])
+    #         except PermissionError:
+    #             pass
+    #         except FileNotFoundError:
+    #             pass
+    #     clear_log_file()
+
+    #     def clean_analyzed_dir_before_running():
+    #         try:
+    #             shutil.rmtree(self.analyzer_data_dir)
+    #         except Exception as e:
+    #             print("Exception "+str(e))
+    #             pass
+    #     clean_analyzed_dir_before_running()
+
+    #     # process = CrawlerProcess(settings=settings)
+    #     process = CrawlerRunner(settings=settings)
+        
+
+    #     spider = ScoringSpider  # CrawlerProcess accepts Spider class or Crawler instances
+    #     sitemapspider = ScoringSpiderSitemap
+
+    #     spider.start_urls = urls
+    #     spider.allowed_domains = self.allowed_domains 
+    #     spider.analyzer = Analyzer(self.analyzer_data_dir)
+    #     sitemapspider.allowed_domains = self.allowed_domains
+    #     sitemapspider.sitemap_urls = sitemap_urls
+    #     sitemapspider.analyzer = spider.analyzer
+
+    #     # process.crawl(sitemapspider)
+    #     # process.crawl(spider)
+    #     @defer.inlineCallbacks
+    #     def crawl():
+    #         yield process.crawl(sitemapspider)
+    #         yield process.crawl(spider)
+    #         print("Crawling in twisted done")
+    #         return self.get_current_stats()
+    #         # reactor.stop()
+    #     crawl()
+    #     print("Crawling() in twisted done")
+    #     reactor.run()
+    #     # process.start()
+    #     # process.stop()
+
+    #     for domain in self.allowed_domains:
+    #         stats = self.reporter.get_stats(domain)
+    #         # stats: 'language_balance', 'lang_count', 'langs', # For full list see Reporter
+    #         score = self.reporter.get_score_from_stats(stats)
+    #         print("Domain: {}, score: {:0.3f}, langs: {}".format(domain, score, stats['langs']))
+
+
+    #     return score    # returns last score for testing
+
+    def get_current_stats(self):
+        try: 
+            allowed_domains = self.allowed_domains
+        except AttributeError:
+            return "No stats - nothing to analyze. Maybe scoring tool not yet initialized"
+        current_status = {}
+        for domain in self.allowed_domains:
+            stats = self.reporter.get_stats(domain)
+            # stats: 'language_balance', 'lang_count', 'langs', # For full list see Reporter
+            score = self.reporter.get_score_from_stats(stats)
+            print("Domain: {}, score: {:0.3f}, langs: {}".format(domain, score, stats['langs']))
+            current_status[domain] = (score, stats)
+        # return (score, stats)
+        return current_status
+    
+    def do_crawling_in_separate_thread(self):
+        # https://stackoverflow.com/questions/14274916/execute-twisted-reactor-run-in-a-thread/14282640
+        # process = CrawlerProcess(settings=settings)
+        process = CrawlerRunner(settings=self.settings)
+        
+
+        spider = ScoringSpider  # CrawlerProcess accepts Spider class or Crawler instances
+        sitemapspider = ScoringSpiderSitemap
+
+        spider.start_urls = self.urls
+        spider.allowed_domains = self.allowed_domains 
+        spider.analyzer = Analyzer(self.analyzer_data_dir)
+        sitemapspider.allowed_domains = self.allowed_domains
+        sitemapspider.sitemap_urls = self.sitemap_urls
+        sitemapspider.analyzer = spider.analyzer
+
+        # process.crawl(sitemapspider)
+        # process.crawl(spider)
+        @defer.inlineCallbacks
+        def crawl():
+            yield process.crawl(sitemapspider)
+            yield process.crawl(spider)
+            print("Crawling in twisted done") # Actual crawl done
+            # reactor.stop()
+
+        crawl()
+        print("Crawling() in twisted done")
+
+        # reactor.run()
+        threading.Thread(target=reactor.run, args=(False,)).start()
+
+        # process.start()
+        # process.stop()
+
+        # for domain in self.allowed_domains:
+        #     stats = self.reporter.get_stats(domain)
+        #     # stats: 'language_balance', 'lang_count', 'langs', # For full list see Reporter
+        #     score = self.reporter.get_score_from_stats(stats)
+        #     print("Domain: {}, score: {:0.3f}, langs: {}".format(domain, score, stats['langs']))
+
+
+        # return score    # returns last score for testing
+
+
+    def initialize4thr(self, urls):
         self.urls = urls
 
-        sitemap_urls = []
+        self.sitemap_urls = []
         for url in urls:
             parsed_url = urlparse(url)
-            sitemap_urls.append(f'{parsed_url.scheme}://{parsed_url.netloc}/sitemap.xml')
-            sitemap_urls.append(f'{parsed_url.scheme}://{parsed_url.netloc}/robots.txt')
-
-        allowed_domains = [extractDomain(i) for i in urls]
-        # print(f'\nPrepared allowed_domains {allowed_domains}\n')
+            self.sitemap_urls.append(f'{parsed_url.scheme}://{parsed_url.netloc}/sitemap.xml')
+            self.sitemap_urls.append(f'{parsed_url.scheme}://{parsed_url.netloc}/robots.txt')
 
         # settings = {}
-        settings = get_project_settings()
+        self.settings = get_project_settings()
 
         config = configparser.ConfigParser()
         config.read('settings.ini')
         def override_default_crawler_config():
             for key in config['crawler']:
-                settings[key.upper()] = config.get('crawler', key, fallback='')
+                self.settings[key.upper()] = config.get('crawler', key, fallback='')
         override_default_crawler_config()
 
-        # Dump config to file for debug
-        with open('settings.cfg', 'w', encoding='utf-8') as of:
-            for key, value in settings.items():
-                of.write(f'{key}, {value}\n')
+        self.analyzer_data_dir = config.get('analyzer', 'data_dir', fallback='')
+        self.reporter = Reporter(self.analyzer_data_dir)
+        self.allowed_domains = [extractDomain(i) for i in urls]
+        # print(f'\nPrepared allowed_domains {allowed_domains}\n')
 
-        # Use new log file for each run
-        try:
-            os.remove(settings['LOG_FILE'])
-        except PermissionError:
-            pass
-        except FileNotFoundError:
-            pass
+        def dump_config_to_file_for_debug():
+            with open('settings.cfg', 'w', encoding='utf-8') as of:
+                for key, value in self.settings.items():
+                    of.write(f'{key}, {value}\n')
+        dump_config_to_file_for_debug()
 
-        analyzer_data_dir = config.get('analyzer', 'data_dir', fallback='')
-        # Clean analyzed_dir before running
-        try:
-            shutil.rmtree(analyzer_data_dir)
-        except Exception as e:
-            print("Exception "+str(e))
-            pass
+        def clear_log_file(): # Use new log file for each run
+            try:
+                os.remove(self.settings['LOG_FILE'])
+            except PermissionError:
+                pass
+            except FileNotFoundError:
+                pass
+        clear_log_file()
 
-        process = CrawlerProcess(settings=settings)
-
-        spider = ScoringSpider  # CrawlerProcess accepts Spider class or Crawler instances
-        sitemapspider = ScoringSpiderSitemap
-
-        spider.start_urls = urls
-        spider.allowed_domains = allowed_domains 
-        spider.analyzer = Analyzer(analyzer_data_dir)
-        sitemapspider.allowed_domains = allowed_domains
-        sitemapspider.sitemap_urls = sitemap_urls
-        sitemapspider.analyzer = spider.analyzer
-
-
-        process.crawl(sitemapspider)
-        process.crawl(spider)
-        process.start()
-        # process.stop()
-
-        reporter = Reporter(analyzer_data_dir)
-        for domain in allowed_domains:
-            stats = reporter.get_stats(domain)
-            # stats: 'language_balance', 'lang_count', 'langs', # For full list see Reporter
-            score = reporter.get_score_from_stats(stats)
-            print("Domain: {}, score: {:0.3f}, langs: {}".format(domain, score, stats['langs']))
-
-
-        return score    # returns last score for testing
-
+        def clean_analyzed_dir_before_running():
+            try:
+                shutil.rmtree(self.analyzer_data_dir)
+            except Exception as e:
+                print("Exception "+str(e))
+                pass
+        clean_analyzed_dir_before_running()
