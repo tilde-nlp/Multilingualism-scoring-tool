213a2845b553a232e1a436c6ef7dcb5874881bc0
Rinalds Vīksna <rinalds.viksna@Tilde.lv>
2021-06-08 13:57:07 +0300

    One more status condition

--- modules/scoring_tool.py
+++ modules/scoring_tool.py
@@ -73,6 +73,8 @@ class ScoringTool():
                 self.status = "ready"
             elif len(self.process.crawlers) > 0 and self.status=="stopping":
                 self.status = "stopping"
+            elif self.p.is_alive():
+                self.status = "stopping"
             elif len(self.process.crawlers) > 0:
                 self.status = "crawling"
             else:
